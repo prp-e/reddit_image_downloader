cc4f6d346e1cb65a7a8b2454e06d73275e060e53
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-09-24 22:13:47 +0330

    Testing and making the damn thing a better tool

--- lib/reddit_image_downloader.rb
+++ lib/reddit_image_downloader.rb
@@ -1,9 +1,19 @@
 require 'json'
-require 'http'
+require 'httparty'
 
 class RedditImage
-	def initialize(sub, qty)
+	def initialize(sub, qty=25)
 		@sub = sub
 		@qty = qty
+	end
+
+	def get_info
+		puts "Going to crawl: "
+		puts "https://reddit.com/r/#{@sub}"
+	end
+
+	def download_images
+		response = HTTParty.get("https://reddit.com/r/#{@sub}.json?limit=#{@qty}")
+		return response.body 
 	end
 end
