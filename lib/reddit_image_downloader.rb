0ce0de2e269f5429115ceae99e6ecbfdde4f4d52
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-09-24 22:56:18 +0330

    Now, it works!

--- lib/reddit_image_downloader.rb
+++ lib/reddit_image_downloader.rb
@@ -1,10 +1,12 @@
 require 'json'
 require 'httparty'
+require 'digest'
 
 class RedditImage
 	def initialize(sub, qty=25, directory="images")
 		@sub = sub
 		@qty = qty
+		@directory = directory
 	end
 
 	def get_info
@@ -15,12 +17,16 @@ class RedditImage
 	def download_images
 		response = HTTParty.get("https://reddit.com/r/#{@sub}.json?limit=#{@qty}", headers: {"User-agent" => "Reddit Image Downloader 1.0"})
 		json_response = JSON.parse(response.body)
-		data = json_response['data']['children']
-		links = [] 
+		data = json_response['data']['children'] 
+
+		Dir::mkdir("#{@directory}")
+		Dir::chdir("#{@directory}")
+
 		data.each do |datum|
 			datum = datum['data']
-			links << datum['url_overridden_by_dest']
-		end 
-		return links 
+			final_image = File.open(Digest::MD5.hexdigest(datum['url_overridden_by_dest']), 'w')
+			final_image.write(HTTParty.get(datum['url_overridden_by_dest']))
+			final_image.close
+		end
 	end
 end
